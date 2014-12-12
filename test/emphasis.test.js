69e524b5ab3581da68966babde2bfab7a17ec38f
Pierre Carion <pcarion@outlook.com>
2014-12-12 14:49:45 +0000

    add new emphasis parsing tests

--- test/emphasis.test.js
+++ test/emphasis.test.js
@@ -10,8 +10,8 @@ var util = require('util');
 
 var jouvence = require("../lib").testMode();
 
-describe('Jouvence', function() {
-    describe('emphasis parsing', function() {
+describe('Jouvence - emphasis parsing', function() {
+    describe('simple string parsing', function() {
         it('should parse a regular line (1)', function() {
             var part = jouvence.__parseEmphasis("allo");
             var parts = part.parts;
@@ -32,7 +32,6 @@ describe('Jouvence', function() {
 
         it('should parse a line with "*"', function() {
             var part = jouvence.__parseEmphasis("how are *you* doing ?");
-            var parts = part.parts;
 
             expect(part).to.eql({
                 type: '.',
@@ -53,7 +52,6 @@ describe('Jouvence', function() {
         });
         it('should parse a line with "**"', function() {
             var part = jouvence.__parseEmphasis("how are **you** doing ?");
-            var parts = part.parts;
 
             expect(part).to.eql({
                 type: '.',
@@ -74,7 +72,6 @@ describe('Jouvence', function() {
         });
         it('should parse a line with "***"', function() {
             var part = jouvence.__parseEmphasis("how are ***you*** doing ?");
-            var parts = part.parts;
 
             expect(part).to.eql({
                 type: '.',
@@ -95,7 +92,6 @@ describe('Jouvence', function() {
         });
         it('should parse a line with "_"', function() {
             var part = jouvence.__parseEmphasis("how are _you_ doing ?");
-            var parts = part.parts;
 
             expect(part).to.eql({
                 type: '.',
@@ -113,6 +109,78 @@ describe('Jouvence', function() {
                     text: " doing ?"
                 }, ]
             });
+        });
+    });
+
+    describe("embedded parsing", function() {
+        it("should parse emphasis inside emphasis", function() {
+            var part = jouvence.__parseEmphasis("how *are _you_ doing* ?");
+            expect(part).to.eql({
+                type: '.',
+                parts: [{
+                    type: '.',
+                    text: "how "
+                }, {
+                    type: '*',
+                    parts: [{
+                        type: '.',
+                        text: 'are '
+                    }, {
+                        type: '_',
+                        parts: [{
+                            type: '.',
+                            text: "you"
+                        }]
+                    }, {
+                        type: '.',
+                        text: ' doing'
+                    }]
+                }, {
+                    type: '.',
+                    text: " ?"
+                }, ]
+            });
+        });
+        it("should parse 2 non embedded emphasis", function() {
+            var part = jouvence.__parseEmphasis("how *are* _you_ doing ?");
+            expect(part).to.eql({
+                type: '.',
+                parts: [{
+                    type: '.',
+                    text: "how "
+                }, {
+                    type: '*',
+                    parts: [{
+                        type: '.',
+                        text: 'are'
+                    }]
+                }, {
+                    type: '.',
+                    text: ' '
+                }, {
+                    type: '_',
+                    parts: [{
+                        type: '.',
+                        text: "you"
+                    }]
+                }, {
+                    type: '.',
+                    text: ' doing ?'
+                }]
+            });
+        });
+    });
+    describe("incomplete emphasis", function() {
+        it("should parse non closed emphasis", function() {
+            var part = jouvence.__parseEmphasis("how *are you doing ?");
+            expect(part).to.eql({
+                type: '.',
+                parts: [{
+                    type: '.',
+                    text: "how *are you doing ?"
+                }]
+            });
+
         });
     });
 });
